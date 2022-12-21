a965235b2ecd9e8fce77a0235e74dc62958db8cb
MindulaHewadewage <mindufernando@gmail.com>
2022-12-21 15:23:28 +0100

    stampo in console i dati

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log ('JS OK')
+const informations =
+    {
+        workers:[
+            {name: 'Wayne Barnett', role:'Founder & CEO', imageUrl:'wayne-barnett-founder-ceo.jpg'},
+            {name: 'Angela Caroll', role:'Chief Editor', imageUrl:'angela-caroll-chief-editor.jpg'},
+            {name: 'Walter Gordon', role:'Office Manager', imageUrl:'walter-gordon-office-manager.jpg'},
+            {name: 'Angela Lopez', role:'Social Media Manager', imageUrl:'angela-lopez-social-media-manager.jpg'},
+            {name: 'Scott Estrada', role:'Developer', imageUrl:'scott-estrada-developer.jpg'},
+            {name: 'Barbara Ramos', role:'Graphic Designer', imageUrl:'barbara-ramos-graèhic-designer.jpg'},
+        ]
+    }
+
+console.log(informations.workers);
